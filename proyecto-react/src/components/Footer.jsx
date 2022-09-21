deab9e66d3d466c27f43879be290d32ec9bd6ea0
Thomas Lagrutta <thomas.lagrutta1@gmail.com>
2022-09-21 16:58:21 -0300

    primer commit react

--- proyecto-react/src/components/Footer.jsx
+++ proyecto-react/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className='pie pieconf txtAb__A'>
             <div className='txtfooter'>
             <p>Derechos de Autor reservados 2022®</p>
-            <p>Diseñado por Thimi  <img src="../public/images/logo.webp"/> </p>
+            <p>Diseñado por Thimi  <img className='footer_img' src="../images/logo.webp"/> </p>
             </div>
         </footer>
     </div>
